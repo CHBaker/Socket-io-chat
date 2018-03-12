d4694f1f10ade46b66bfa16e29bebe4c42d6ce42
Charles Hudson Baker <charles.baker@besl.org>
2018-03-12 10:02:48 -0600

    FEATURE: chat function
    - Allows users to talk over chat

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ io.on('connection', function(socket) {
     console.log('a user connected');
 
     socket.on('chat message', function(msg) {
-        console.log('message: ', msg);
+        io.emit('chat message', msg);
+        console.log(msg)
     });
 
     socket.on('disconnect', function() {
